fb45127d96e4eef20e024d5303a44cafa8300379
farhansaif <farhansaif488@gmail.com>
2022-06-07 20:06:39 +0600

    added authentication and navbar

---
+++ project/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,url_for,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = "abc"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+
+    # blueprint for auth rules in our app
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
